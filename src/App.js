ecbc01670296a5ad4f2c924c39b4a7a8992dcd24
chibuz0r <chibuzc@yahoo.co.uk>
2020-04-01 03:07:07 +0100

    test login

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import {useUser} from './context/userProvider'
+import { useAuth } from './context/authProvider';
 
 function App() {
+
+  useAuth().login()
+  // useEffect(login , [] )
 
   const user = useUser();
   console.log('user in app', user);
--- src/context/authProvider.js
+++ src/context/authProvider.js
@@ -4,15 +4,18 @@ import { AuthService } from "../services/authService";
 const AuthContext = React.createContext();
 
 const AuthProvider = (props) => {
+
+
   //run this function immediately;
   //probably using the hook that replaces componentDidMount
   //useEffect
+ 
   const user = AuthService.getUser();
   console.log('user in authProvider', user);
 
-  const login = async () => AuthService.login();
-  const register = async () => AuthService.register();
-  const logout = async () => AuthService.logout();
+  const login =  () => AuthService.login();
+  const register =  () => AuthService.register();
+  const logout =  () => AuthService.logout();
 
   return <AuthContext.Provider value={{ user, login, register, logout }} {...props} />;
 };
